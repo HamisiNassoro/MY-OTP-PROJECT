8be3b83b28867a7a2b052a952ffbdbd9c74e7de5
hamso <hamsonassor14@gmail.com>
2022-01-26 17:20:28 +0300

    modified the mobile field to have PhoneNumber field

--- verification/models.py
+++ verification/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
 #this model stores the data of the Phones verified
 class phoneModel(models.Model):
-    Mobile = models.IntegerField(blank=False)
+    Mobile = PhoneNumberField(blank=True)
     isVerified = models.BooleanField(blank=False, default=False)
     counter = models.IntegerField(default=0, blank=False)
     
