d14f131f40a19a4957ad4c316c3da340ffefce0d
hamso <hamsonassor14@gmail.com>
2022-03-10 19:21:44 +0300

    added username field

--- verification/models.py
+++ verification/models.py
@@ -6,6 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 #this model stores the data of the Phones verified
 class phoneModel(models.Model):
     Mobile = models.IntegerField(blank=False)
+    username = models.CharField(max_length=255)
     isVerified = models.BooleanField(blank=False, default=False)
     counter = models.IntegerField(default=0, blank=False)
     
